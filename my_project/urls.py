9e50d9310ac2178fb7fff83ba7fc422ddc433caf
muazainal <muazainal@live.com>
2026-02-13 12:24:54 +0000

    modified urls.py

--- my_project/urls.py
+++ my_project/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from hello_world import views as index_views
 
 urlpatterns = [
-    path('hello', index_views.index, name='index'),
+    path('', index_views.index, name='home'),      # homepage
+    path('hello/', index_views.index, name='index'), # /hello/ route
     path('admin/', admin.site.urls),
 ]
